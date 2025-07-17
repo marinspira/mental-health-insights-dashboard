801045e67e1a86a039fbbf21940c551cf3a045d3
Maria Eduarda <marinspiraa@gmail.com>
2025-07-17 11:51:05 -0300

    Create analyze emotions POST endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,38 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 from transformers import pipeline
 
-# This model uses safetensors, safe to load without torch>=2.6
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "https://marinspira.github.io/mental-health-insights-dashboard/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Load emotion model pipeline
 emotion_classifier = pipeline(
     "text-classification",
     model="SamLowe/roberta-base-go_emotions",
     top_k=None
 )
 
-text = "I've been feeling very low and tired lately."
-results = emotion_classifier(text)
+# Request schema
+class NoteInput(BaseModel):
+    text: str
 
-for emotion in results[0]:
-    print(f"{emotion['label']}: {emotion['score']:.2f}")
+# POST endpoint
+@app.post("/analyze-emotions")
+async def analyze_emotions(note: NoteInput):
+    results = emotion_classifier(note.text)
+    return {"emotions": results[0]}
+
+# Initial test
+# text = "I've been feeling very low and tired lately."
+# results = emotion_classifier(text)
+
+# for emotion in results[0]:
+#     print(f"{emotion['label']}: {emotion['score']:.2f}")
